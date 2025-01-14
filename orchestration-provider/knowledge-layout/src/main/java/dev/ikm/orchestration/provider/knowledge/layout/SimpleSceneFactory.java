ed44c21ab606e17c9bdf5d2c6a92224c0c67523f
Keith E Campbell <rotors.snooty-0e@icloud.com>
2025-01-14 10:35:36 -0800

    Checkpoint for new library…

--- orchestration-provider/knowledge-layout/src/main/java/dev/ikm/orchestration/provider/knowledge/layout/SimpleSceneFactory.java
+++ orchestration-provider/knowledge-layout/src/main/java/dev/ikm/orchestration/provider/knowledge/layout/SimpleSceneFactory.java
@@ -1,28 +1,37 @@
 package dev.ikm.orchestration.provider.knowledge.layout;
 
 import dev.ikm.komet.layout.KlWidget;
+import dev.ikm.komet.layout.preferences.KlPreferencesFactory;
+import dev.ikm.komet.layout.window.KlScene;
 import dev.ikm.komet.layout.window.KlSceneFactory;
 import dev.ikm.komet.preferences.KometPreferences;
 import javafx.scene.Scene;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.function.Supplier;
+
 public class SimpleSceneFactory implements KlSceneFactory {
 
     private static final Logger LOG = LoggerFactory.getLogger(SimpleSceneFactory.class);
 
     @Override
-    public Scene create(KometPreferences preferences) {
-        return null;
+    public KlScene create(KlWidget widget, Supplier<KometPreferences> preferenceSupplier) {
+        return new KlSceneRecord(new Scene(widget.klWidget()), widget, preferenceSupplier.get());
     }
 
     @Override
-    public Class<? extends KlWidget> klWidgetInterfaceClass() {
-        return null;
+    public KlScene restore(KometPreferences preferences) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
-    public Class<?> klWidgetImplementationClass() {
-        return null;
+    public Class klInterfaceClass() {
+        return KlScene.class;
+    }
+
+    @Override
+    public Class klImplementationClass() {
+        return KlSceneRecord.class;
     }
 }
--- orchestration-provider/knowledge-layout/src/main/java/dev/ikm/orchestration/provider/knowledge/layout/SimpleWindowFactory.java
+++ orchestration-provider/knowledge-layout/src/main/java/dev/ikm/orchestration/provider/knowledge/layout/SimpleWindowFactory.java
@@ -1,41 +1,43 @@
 package dev.ikm.orchestration.provider.knowledge.layout;
 
 import dev.ikm.komet.layout.KlWidget;
+import dev.ikm.komet.layout.KlWidgetFactory;
+import dev.ikm.komet.layout.preferences.KlPreferencesFactory;
+import dev.ikm.komet.layout.window.KlScene;
 import dev.ikm.komet.layout.window.KlSceneFactory;
 import dev.ikm.komet.layout.window.KlWindow;
 import dev.ikm.komet.layout.window.KlWindowFactory;
 import dev.ikm.komet.preferences.KometPreferences;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.controlsfx.control.action.Action;
+import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.function.Supplier;
+
 public class SimpleWindowFactory implements KlWindowFactory {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleWindowFactory.class);
 
-    @Override
-    public KlWindow create(KometPreferences preferences) {
-        return null;
-    }
+    private final SimpleSceneFactory sceneFactory = new SimpleSceneFactory();
 
-    @Override
-    public KlWindow create() {
-        return null;
-    }
 
     @Override
     public ImmutableList<Action> createNewWindowActions(KlSceneFactory sceneFactory) {
-        return null;
+        return Lists.immutable.empty();
     }
 
     @Override
     public KlWindow restore(KometPreferences preferences) {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public ImmutableList<Action> createRestoreWindowActions() {
-        return null;
+        return Lists.immutable.empty();
     }
 
     @Override
@@ -49,12 +51,26 @@ public class SimpleWindowFactory implements KlWindowFactory {
     }
 
     @Override
+    public KlWindow create(KlWidgetFactory widgetFactory) {
+        KometPreferences windowPreferences = KlPreferencesFactory.createWindowPreferences(KlWindowRecord.class);
+        KlWidget widget = widgetFactory.create(KlPreferencesFactory.createFactory(windowPreferences,
+                widgetFactory.klImplementationClass()));
+
+        KlScene scene = sceneFactory.create(widget,
+                KlPreferencesFactory.createScenePreferencesSupplier(windowPreferences));
+        Stage window = new Stage();
+        window.setScene(scene.scene());
+        KlWindowRecord windowRecord = new KlWindowRecord(window, scene, windowPreferences);
+        return windowRecord;
+    }
+
+    @Override
     public Class klInterfaceClass() {
-        return ;
+        return KlWindow.class;
     }
 
     @Override
     public Class klImplementationClass() {
-        return null;
+        return KlWindowRecord.class;
     }
 }
